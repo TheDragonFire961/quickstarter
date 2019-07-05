9f98dd311439a8206c2f8be2a608602ccdb68eea
TheDragonFire961 <37818652+TheDragonFire961@users.noreply.github.com>
2019-07-06 08:43:07 +1000

    Update loader.js

--- loader.js
+++ loader.js
@@ -20,7 +20,7 @@ class Loader {
     user_parameters() {
         this.logo = "";
         this.target_id = "quickstarter";
-	this.loader_mode = "manual_start" // Modes: auto, manual, manual_start_client, manual_start_loader.
+	    this.loader_mode = "manual_start" // Modes: auto, manual, manual_start_client, manual_start_loader.
         this.splash_mode = "auto" // Modes: auto, diagnostic, none, standard.
         this.log_type = "html"; // Log types: none, console, html, all.
         // files_to_load = ""
@@ -30,31 +30,36 @@ class Loader {
         // Try constructing the relevant tags
         try {
             this.target = document.getElementById(this.target_id);
-            this.target.innerHTML = '<div class="quickloader_nodisplay" id="quickloader_console"></div>';
-            this.console = document.getElementById("quickloader_console");
+            this.target.innerHTML = '<div class="quickstarter_body" id="quickstarter_body"></div>
+            this.body = document.getElementById("quickstarter_body");
+            this.body.innerHTML = '<div class="quickstarter_nodisplay" id="quickstarter_console"></div>';
+            this.console = document.getElementById("quickstarter_console");
+	        this.date = new Date();
         } catch (error) {
-	    // Well, shit, we don't have a console yet, so...
-            alert("A fatal exception occured while initialising the loader. Please report this error code to this website's owner: " + error;
+	        // Well, shit, we don't have a console yet, so...
+            alert("A fatal exception occured while initialising the loader. Please report this error code to this website's owner: " + error);
         }
         // Try constructing classes we need 
         try {
 	        this.Log('Initialiser', 'INFO', 'Console log intialisation complete.');
             this.Log('Initialiser', 'INFO', 'Constructing class dependencies');
-            this.date = new Date();
         } catch (error) {
-            this.Log('Initialiser'
+            this.Log('Initialiser', 'ERROR', 'Error initialising class dependencies: ' + error);
         }
     }
     Initialise() {
         // Create a list of files
     }
     Log(thread, level, message) {
-        log_message = (this.date.GetDay() + 1) + '-' + (this.date.GetMonth() + 1) + '-' (this.date.GetFullMonth() + 1) + ' ' + this.date.GetHours() + this.date.GetMinutes() + this.date.GetSeconds();
+        var log_message = ('0' + this.date.getDay()).slice(-2) + '-' + ('0' + (this.date.getMonth() + 1)).slice(-2) + '-' + this.date.getFullYear() + ' ' + ('0' + this.date.getHours()).slice(-2) + ':' + ('0' + this.date.getMinutes()).slice(-2) + ':' + ('0' + this.date.getSeconds()).slice(-2);
         log_message += ' ';
         log_message += '[' + thread + '] ';
         log_message += '[' + level + '] ';
         log_message += message;
         // Write to HTML, if that is enabled.
+        if (this.log_type === "html" || this.log_type === "all") {
+            this.console = log_message + '<br>';
+        }
         if (this.log_type === "html" || this.log_type === "all") {
             this.console = log_message + '<br>';
         }
